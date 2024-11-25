dd6b368fb5d3d369d92edf8a615b04c79f79f645
heheboizzzz <116245563+heheboizzzz@users.noreply.github.com>
2024-11-25 20:51:43 +0700

    add quantity in ultilities detail request

--- src/screens/InitialQuotationScreen/ConfirmInformation.tsx
+++ src/screens/InitialQuotationScreen/ConfirmInformation.tsx
@@ -102,6 +102,7 @@ const ConfirmInformation: React.FC = () => {
         utilitiesItemId: item.checkedItems ?? item.id,
         name: item.checkedItemName ?? item.name,
         price: item.totalPrice,
+        quantity: item.area === '' ? null : item.area,
       }),
     );
 
