7dde36e8e4ae1741ea39ad30be7a017023a1ce24
heheboizzzz <116245563+heheboizzzz@users.noreply.github.com>
2024-11-25 20:58:10 +0700

    change API URL endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,1 @@
-export const API_ROOT = "https://rhqs-fzbda8gefsh7bnft.southeastasia-01.azurewebsites.net/api/v1";
-export const API_UPLOAD = "https://rhqs-fzbda8gefsh7bnft.southeastasia-01.azurewebsites.net/api/UploadImage/api";
+export const API_ROOT = "https://rhcqs-b4brchgaeqb9abd5.southeastasia-01.azurewebsites.net/api/v1";
